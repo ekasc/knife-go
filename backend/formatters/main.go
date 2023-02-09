f06a6787cbefa1af8ad7e69f016d54881558bd13
ekasc <ekas.esc@gmail.com>
2023-02-10 05:02:43 +0530

    added formatter endpoint
    basic functionality for now. Will improve it later.

--- backend/formatters/main.go
+++ backend/formatters/main.go
@@ -1,19 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Request struct {
+	Type    string `json:"type"`
+	Data    string `json:"data"`
+	Options int    `json:"options"`
+}
 
 func main() {
-	jason := `{"Services":[{"Orders":[{"ID":"$save ID1","SupplierOrderCode":"$SupplierOrderCode"},{"ID":"$save ID2","SupplierOrderCode":111111}]}]}`
-	i, err := FormatJSON(jason, 3)
-	if err != nil {
-		fmt.Println(err)
-	}
-	_ = i
+	http.HandleFunc("/format", format1)
 
-	i = MiniCSS("")
-	if err != nil {
-		fmt.Println(err)
-	}
+	http.ListenAndServe(":8080", nil)
+}
 
-	i = FormatHTML("")
+func format1(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method == "POST" {
+		p := Request{}
+
+		if r.Body == nil {
+			fmt.Println("body is empty")
+		}
+
+		dd := json.NewDecoder(r.Body)
+		dd.UseNumber()
+		err := dd.Decode(&p)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+		}
+		defer r.Body.Close()
+
+		payload := Format(p)
+
+		json.NewEncoder(w).Encode(payload)
+	}
 }
