7eb2e398863aec13ffb2496b2d8a7358bc52944b
ekasc <ekas.esc@gmail.com>
2023-03-28 02:35:30 +0530

    removed

--- frontend/src/components/nav/bar/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { TextForm } from "../../sections/content/formatters";
-import { Box } from "./box";
-
-export const Layout = ({ children }: { children: any }) => (
-	<Box css={{ maxW: "100%"}} >
-		{children}
-	</Box>
-)
-
